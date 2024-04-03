62c3bf5c8111fd6160ef2597284b3f01dd8ec9a6
allenbeckwith01 <165954287+allenbeckwith01@users.noreply.github.com>
2024-04-03 17:01:16 -0500

    Update data.tsx
    Signed-off-by: allenbeckwith01 <165954287+allenbeckwith01@users.noreply.github.com>

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -73,14 +73,10 @@ export const heroData: Hero = {
   description: (
     <>
       <p className="prose-sm text-stone-200 sm:prose-base lg:prose-lg">
-        I'm a Victoria based <strong className="text-stone-100">Full Stack Software Engineer</strong>, currently working
-        at <strong className="text-stone-100">Instant Domains</strong> helping build a modern, mobile-first, domain
-        registrar and site builder.
+        Network Engineer @ AT&T
       </p>
       <p className="prose-sm text-stone-200 sm:prose-base lg:prose-lg">
-        In my free time time, you can catch me training in <strong className="text-stone-100">Muay Thai</strong>,
-        plucking my <strong className="text-stone-100">banjo</strong>, or exploring beautiful{' '}
-        <strong className="text-stone-100">Vancouver Island</strong>.
+        B.S. Electrical Engineering, Northwestern University
       </p>
     </>
   ),
